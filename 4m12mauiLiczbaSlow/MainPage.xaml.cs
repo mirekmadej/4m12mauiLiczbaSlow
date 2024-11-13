2cca02055934b9bbdd5f56dcaed5be893ef25448
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-11-13 08:40:48 +0100

    poprawione zliczanie słów

--- 4m12mauiLiczbaSlow/MainPage.xaml.cs
+++ 4m12mauiLiczbaSlow/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace _4m12mauiLiczbaSlow
 {
@@ -13,14 +14,28 @@ namespace _4m12mauiLiczbaSlow
 
         private async void btnPoliczSlowa(object sender, EventArgs e)
         {
+            string wyczyscSpacje(string s)
+            {
+                while(s.Contains("  "))
+                {
+                    s = s.Replace("  ", " ");
+                }
+                return s;
+            }
             if (string.IsNullOrEmpty(entNapis.Text))
             {
                 await DisplayAlert("info", "w ciągu jest 0 słów", "rozumiem");
                 return;
             }
-                
+            string napis = wyczyscSpacje(entNapis.Text);
+            napis = napis.Trim();
+            if(napis == " ")
+            {
+                await DisplayAlert("info", "w ciągu jest 0 słów", "rozumiem");
+                return;
+            }
             int licznik = 1;
-            foreach (var c in entNapis.Text)
+            foreach (var c in napis)
                 if (c == ' ')
                     licznik++;
             await DisplayAlert("info", $"liczba słów: {licznik}", "rozumiem");
@@ -54,10 +69,81 @@ namespace _4m12mauiLiczbaSlow
                     wynik = c + wynik;
                 return wynik;
             }
+
+            string napis = entNapis.Text.ToUpper();
+            napis = wyczyscTekst(napis);
             string p = "Napis nie jest palindromem";
-            if(odwrocNapis(entNapis.Text) == entNapis.Text)
+            if(odwrocNapis(napis) == napis)
                 p = "Napis jest palindromem";
             await DisplayAlert("info", p,"rozumiemi");
+            //entNapis.Text = napis;
+        }
+        private async void btnCzyAnagram(object sender, EventArgs e)
+        {
+            // drugi pomysł pomysł: usuwać znaki z pierwszego stringu w drugim
+            //  
+            if (string.IsNullOrEmpty(entAnagram1.Text) || string.IsNullOrEmpty(entAnagram1.Text))
+            {
+                await DisplayAlert("info", "pusty napis nie jest anagramem", "rozumiem");
+                return;
+            }
+            string s1 = wyczyscTekst(entAnagram1.Text.ToUpper());
+            string s2 = wyczyscTekst(entAnagram2.Text.ToUpper());
+            if(s1.Length != s2.Length)
+            {
+                await DisplayAlert("info", "to nie anagramy", "ok");
+                return;
+            }
+            foreach(var c in s1)
+            {
+                int poz = s2.IndexOf(c);
+                if(poz == -1)
+                {
+                    await DisplayAlert("info", "to nie anagramy", "ok");
+                    return;
+                }
+                s2 = s2.Remove(poz, 1);
+            }
+            string p = "to nie jest anagram";
+            if(s2.Length == 0)
+                p = "to jest anagram";
+            await DisplayAlert("info", p, "ok");
+
+        }
+        private async void btnCzyAnagram1(object sender, EventArgs e)
+        {
+            // pierwszy pomysł: zamienić na tablice, posortować,
+            // zamienić na string  i porównać 
+            if (string.IsNullOrEmpty(entAnagram1.Text) || string.IsNullOrEmpty(entAnagram1.Text))
+            {
+                await DisplayAlert("info", "pusty napis nie jest anagramem", "rozumiem");
+                return;
+            }
+            string s1 = wyczyscTekst(entAnagram1.Text.ToUpper());
+            string s2 = wyczyscTekst(entAnagram2.Text.ToUpper());
+            char[] t1 = s1.ToCharArray();
+            Array.Sort(t1);
+            char[] t2 = s2.ToCharArray();
+            Array.Sort(t2);
+            s1 = s2 = "";
+            foreach (var c in t1)
+                s1 += c;
+            foreach (var c in t2)
+                s2 += c;
+            string p = "to nie jest anagram";
+            if(s1 == s2)
+                p = "to jest anagram";
+            await DisplayAlert("info", p, "ok");
+        }
+
+
+        private static string wyczyscTekst(string s)
+        {
+            string znaki = " ,.?!";
+            foreach (var c in znaki)
+                if (s.Contains(c))
+                    s = s.Replace(c.ToString(), "");
+            return s;
         }
     }
 
